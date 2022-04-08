9333a45d29f2cf728144ebdd28f2c647061eca11
Valentin Ivanov <valentinivanov@Air-Valentin.fritz.box>
2022-04-08 13:37:46 +0300

    log in redy

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome Page</title>
+</head>
+<body>
+<div class="container">
+    <br></br><br></br><br></br><h2 class="success">Welcome Admin!!!</h2>
+    <a href="login.php">Logout</a>
+</div>
+    
+</body>
+</html>
